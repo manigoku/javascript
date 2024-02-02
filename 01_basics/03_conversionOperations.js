54c579f6a6fc84a452218f637b79353599804bcb
Mani gokul <156907342+manigoku@users.noreply.github.com>
2024-02-02 10:47:13 +0000

    conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let score = null
+
+//console.log(score);
+//console.log(typeof score);
+
+let valueInNumber = Number (score)
+//console.log(typeof valueInNumber );
+//console.log(valueInNumber);
+
+// 33=>33
+// 33abc => NaN
+// true => 1; false=>0;
+
+let isLoggedIn = "mani"
+
+let booleanIsLoogedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoogedIn);
+// true => 1; false=>0;
+// string = true
+
+let sn = 25
+let usn = String(sn)
+console.log(typeof usn);
