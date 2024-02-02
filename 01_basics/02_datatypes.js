e19ae712342bfdbb0975b66a365e661a4903492f
Mani gokul <156907342+manigoku@users.noreply.github.com>
2024-02-02 10:22:37 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+
+"use strict"; // treat all js code as newer version
+
+console.log(3+3);
+console.log("mani"); // code readability should be high
+
+
+
+
+//primitive type
+/*
+number,string,boolean,null,undefined,bigint,symbol
+*/
+
+// non primitive
+ /* objects     arrays ,functions */
