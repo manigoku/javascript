3ee499a0ba49c5415e4e7b0a48dbabdaaea16208
Mani gokul <156907342+manigoku@users.noreply.github.com>
2024-02-03 14:21:16 +0000

    numManth

---
+++ 01_basics/05_numMath.js
@@ -0,0 +1,28 @@
+const number = 100
+/* const bal = new Number(120)
+
+/* console.log(bal);
+console.log(bal.toString().length); // length of the string
+console.log(bal.toFixed(2)); // gives accurate decimals  */
+
+/* const number1 = 23.9876
+console.log(number1.toPrecision(4));//outputs 23.99
+const number2 = 1000000
+console.log(number2.toLocaleString(`en-IN`));  */ */// outputs 10,00,000
+
+/// maths ///
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.floor(4.6));// floor,ceil,round for 
+console.log(Math.sqrt(25));
+console.log(Math.max(2,5,3,9,4));//get max and min values
+console.log(Math.random()*6);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(min-max +1)) +min);
+
+
+
