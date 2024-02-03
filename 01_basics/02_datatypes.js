abb6b490ea26f2d22afd107cd27bacd0770231a7
Mani gokul <156907342+manigoku@users.noreply.github.com>
2024-02-03 12:52:58 +0000

    stack and heap

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -14,3 +14,20 @@ number,string,boolean,null,undefined,bigint,symbol
 
 // non primitive
  /* objects     arrays ,functions */
+
+
+ // memories 
+ // stacck (primitive),heap(non primitive)
+
+ let myGfName = "speedy" 
+ let myGf = myGfName
+
+ // all the var values are stored in stack
+
+ let user = {
+    myName :"mani",
+    email:"bjkjabvj@myGf.com"
+
+ }
+
+ // user is stored in stack//and myname and email are stored in heap
