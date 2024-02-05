e51a046d8d5cef11b5457a8da06d4e1ca771f022
Mani gokul <156907342+manigoku@users.noreply.github.com>
2024-02-05 05:29:26 +0000

    const

---
+++ 01_basics/Const.js
@@ -0,0 +1,10 @@
+ const PI = 3.14;
+ let radius;
+ let circumference;
+
+ document.getElementById("mysub").onclick = function(){
+    radius = document.getElementById("mytext").value;
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById("myH2").textContent = circumference;
+ }
