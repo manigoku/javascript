a21d5e48e49e6f84daa6685ce0303d1adb038a53
Mani gokul <156907342+manigoku@users.noreply.github.com>
2024-02-09 15:15:40 +0000

    callback

---
+++ 01_basics/11_callback.js
@@ -0,0 +1,29 @@
+// callback = it is  a function that passed as an argument 
+ //                 to anotther function
+  //              used to handle asyncchronus operations:
+  //               1. reading a file
+  //               2. network requests
+  //               3. Interacting with databeses
+
+  //            hey, when you are done, call the next.
+
+
+
+
+
+ /*  function hello(callback){
+    console.log("hello!");
+    callback();
+  }
+                                          
+  function goodbye (){
+    console.log("good bye!");
+  }
+
+  hello(goodbye); */
+
+
+
+
+   
+
