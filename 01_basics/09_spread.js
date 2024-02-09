18abaa0a630558a5e758357b6199d39f9a08d476
Mani gokul <156907342+manigoku@users.noreply.github.com>
2024-02-09 13:20:24 +0000

    spread operator

---
+++ 01_basics/09_spread.js
@@ -0,0 +1,16 @@
+/* // spread opreator :- (...) allows an iterable such as an array  or string 
+                      // to be expanded into separate elements
+                      // 9unpacks the elements
+
+let numbers =[1,2,3,4,5];
+let max = Math.max(...numbers);
+let min = Math.min(...numbers);
+console.log(min); */
+
+let fruits = [ "apple","mango","banana"];
+let veggies = ["hbac","hdbcka","jkcab"];
+let foods = [...fruits,...veggies,"has","hjsdbc"]; // can be used to unpack all elements in 
+                                                       //arrays nd create one single array
+
+
+console.log(foods);
