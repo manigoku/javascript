f7f5cdef7b6796cf275234ad158661eb1847ef78
Mani gokul <156907342+manigoku@users.noreply.github.com>
2024-02-10 08:38:32 +0000

    functionexpressions

---
+++ 01_basics/16_functionexpression.js
@@ -0,0 +1,15 @@
+/* 
+node 01_basics/16_functionexpression.js */
+
+ // function expressions =  a way of defining function to value or variable
+
+ const numbers = [ 1,2,3,4,5,6];
+
+ const total = numbers.reduce(function(a,b){
+    return a+b;
+ });
+
+ const squares = numbers.map(function(element){
+   return Math.pow(element,2)
+ });
+ console.log(squares);
