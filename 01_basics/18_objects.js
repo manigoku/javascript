a3633a2f3e8416693edb210ef86365a87bc2715f
Mani gokul <156907342+manigoku@users.noreply.github.com>
2024-02-10 09:49:47 +0000

    objects

---
+++ 01_basics/18_objects.js
@@ -0,0 +1,21 @@
+// objects is a collection of properties and methods can represent real world objects
+
+
+
+const person={
+    firstName : "mani",
+    lastName : "speedy",
+    age :25,
+    isEmployed :true,
+    sayhello : function(){
+        console.log(`hai i am mani`)
+    }
+
+}
+const person1={
+    firstName : "manigoku",
+    lastName : "speedyriji",
+    age :27,
+    isEmployed :false
+}
+person.sayhello();
