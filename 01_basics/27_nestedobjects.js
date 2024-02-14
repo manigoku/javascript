5831023f147da204d528c0c8d8be1d4af99a9ac8
Mani gokul <156907342+manigoku@users.noreply.github.com>
2024-02-14 17:07:35 +0000

    nested objects

---
+++ 01_basics/27_nestedobjects.js
@@ -0,0 +1,12 @@
+// nested objects= objects inside of  other objects 
+const person = {
+    firstname:'mani',
+    lastname:"speedy",
+    age:25,
+    address:{
+        doorno:7-114,
+        street:"ramalayastreet",
+        pincode:521110
+    }
+}
+console.log(person.address.pincode);
