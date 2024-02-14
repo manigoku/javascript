bc34146fa10af6373664c77216d7aace4f4a7c35
Mani gokul <156907342+manigoku@users.noreply.github.com>
2024-02-14 16:21:29 +0000

    getters setters

---
+++ 01_basics/25_getset.js
@@ -0,0 +1,35 @@
+/* // getter = a special method that makes a property readable
+// setter = a special mehod makes a property more writable
+class Rectangle{
+    constructor(w,h){
+      this.w = w;
+      this.h = h;
+    }
+    set w(newwidth){
+        if(newwidth>0){
+      this._newwidth=newwidth;
+        }
+        else{
+            console.error(`w must be a positive number`);
+        }
+    }
+    set h(newheight){
+        if(newheight>0){
+      this._newheight=newheight;
+        }
+        else{
+            console.error(`h must be a positive number`);
+        }
+    }
+    get w(){
+        return this._newwidth;
+    }
+    get h(){
+        return this._newheight;
+    }
+    
+}
+const rectangle = new Rectangle(3,4);
+console.log(rectangle.w);
+console.log(rectangle.h);
+ */
